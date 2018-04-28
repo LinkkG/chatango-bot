fa200e1870b2db280d2459839b3d3f9ea7ea62af
José <33236893+TheClonerx@users.noreply.github.com>
2018-04-28 11:42:40 -0300

    Updated .gitignore

--- cmds/hello.py
+++ cmds/hello.py
@@ -2,4 +2,4 @@ import config
 
 cuser = config.get_user(user.name)
 
-room.message(config.get_lang(cuser["lang"], "hello").format(user.name.title()))
+room.message(config.get_lang(cuser["lang"], "hello").format(user.capser))
